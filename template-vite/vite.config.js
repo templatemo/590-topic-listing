0dffb33e12dafe4b6776a1baf6639ff6c5fd2e6c
ChristianP <1445466-poohspear@users.noreply.gitlab.com>
2023-06-06 16:12:49 +0630

    Commit message

--- template-vite/vite.config.js
+++ template-vite/vite.config.js
@@ -2,7 +2,17 @@ const path = require('path')
 
 export default {
   root: path.resolve(__dirname, 'src'),
-  build: {outDir: '../dist'},
+  build: {
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        contact: resolve(__dirname, 'src/contact.html'),
+        main: resolve(__dirname, 'src/index.html'),
+        topics: resolve(__dirname, 'src/topics.html'),
+        topic: resolve(__dirname, 'src/topic.html')
+      }
+    }
+  },
   resolve: {
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
